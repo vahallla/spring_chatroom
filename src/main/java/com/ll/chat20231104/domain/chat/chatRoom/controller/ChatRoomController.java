1ac5a7b6279a2d08655a635d271896b1d9370f55
vahallla <flskxhtm@naver.com>
2023-11-04 13:54:57 +0900

    메시지 방 및 닉네임 표시

--- src/main/java/com/ll/chat20231104/domain/chat/chatRoom/controller/ChatRoomController.java
+++ src/main/java/com/ll/chat20231104/domain/chat/chatRoom/controller/ChatRoomController.java
@@ -2,13 +2,18 @@ package com.ll.chat20231104.domain.chat.chatRoom.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ChatRoomController {
-    @GetMapping("/chat/room/1")
+    @GetMapping("/chat/room/{roomId}")
     @ResponseBody
-    public String showRoom() {
-        return "1번 채팅방 입니다.";
+    public String showRoom(
+            @PathVariable final long roomId,
+            @RequestParam(defaultValue = "youngwoo") final String writerName
+    ) {
+        return "%d번 채팅방 입니다. writer : %s".formatted(roomId, writerName);
     }
 }
