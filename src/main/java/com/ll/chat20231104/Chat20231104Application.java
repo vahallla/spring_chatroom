a0acfe816f3b607c166c569402abff72b5ce52c3
vahallla <flskxhtm@naver.com>
2023-11-04 14:28:35 +0900

    메시지 make.html 추가

--- src/main/java/com/ll/chat20231104/Chat20231104Application.java
+++ src/main/java/com/ll/chat20231104/Chat20231104Application.java
@@ -3,7 +3,6 @@ package com.ll.chat20231104;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Chat20231104Application {
 
--- src/main/java/com/ll/chat20231104/domain/chat/chatRoom/controller/ChatRoomController.java
+++ src/main/java/com/ll/chat20231104/domain/chat/chatRoom/controller/ChatRoomController.java
@@ -1,19 +1,22 @@
 package com.ll.chat20231104.domain.chat.chatRoom.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/chat/room")
 public class ChatRoomController {
-    @GetMapping("/chat/room/{roomId}")
+    @GetMapping("/{roomId}")
     @ResponseBody
     public String showRoom(
             @PathVariable final long roomId,
-            @RequestParam(defaultValue = "youngwoo") final String writerName
+            @RequestParam(defaultValue = "NoName") final String writerName
     ) {
         return "%d번 채팅방 입니다. writer : %s".formatted(roomId, writerName);
+    }
+
+    @GetMapping("/make")
+    public String showMake() {
+        return "domain/chat/chatRoom/make";
     }
 }
